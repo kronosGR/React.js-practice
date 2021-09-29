5f71341a3b2183a3db81384ec91ed311764e458a
KronosGR <geo.elgeo@gmail.com>
2021-09-29 11:39:23 +0200

    2-way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,6 +45,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -52,7 +55,11 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input
+            type='text'
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -61,6 +68,7 @@ const ExpenseForm = () => {
             min='0.01'
             step='0.01'
             onChange={amountChangeHandler}
+            value={enteredAmount}
           />
         </div>
         <div className='new-expense__control'>
@@ -70,6 +78,7 @@ const ExpenseForm = () => {
             min='2019-01-01'
             max='2029-01-01'
             onChange={dateChangeHandler}
+            value={enteredDate}
           />
         </div>
       </div>
